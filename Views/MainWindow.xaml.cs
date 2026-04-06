093a299ec7e5d9f6ed4de00bd5522528e2fa6b71
已逝情殇 <3010605050@qq.com>
2026-04-06 16:43:27 +0800

    优化最大化最小化按钮配色

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -17,14 +17,20 @@ public sealed partial class MainWindow : Window
     private const int MinimumWindowHeight = 720;
     private static readonly Color LightTitleBarBackgroundColor = ColorHelper.FromArgb(255, 243, 243, 243);
     private static readonly Color LightTitleBarForegroundColor = Colors.Black;
+    private static readonly Color LightTitleBarButtonForegroundColor = ColorHelper.FromArgb(255, 96, 96, 96);
+    private static readonly Color LightTitleBarButtonHoverForegroundColor = Colors.White;
+    private static readonly Color LightTitleBarButtonPressedForegroundColor = Colors.White;
     private static readonly Color LightTitleBarInactiveForegroundColor = ColorHelper.FromArgb(255, 112, 112, 112);
-    private static readonly Color LightTitleBarButtonHoverBackgroundColor = ColorHelper.FromArgb(28, 0, 0, 0);
-    private static readonly Color LightTitleBarButtonPressedBackgroundColor = ColorHelper.FromArgb(54, 0, 0, 0);
+    private static readonly Color LightTitleBarButtonHoverBackgroundColor = ColorHelper.FromArgb(255, 122, 122, 122);
+    private static readonly Color LightTitleBarButtonPressedBackgroundColor = ColorHelper.FromArgb(255, 102, 102, 102);
     private static readonly Color DarkTitleBarBackgroundColor = ColorHelper.FromArgb(255, 31, 31, 31);
     private static readonly Color DarkTitleBarForegroundColor = Colors.White;
+    private static readonly Color DarkTitleBarButtonForegroundColor = ColorHelper.FromArgb(255, 214, 214, 214);
+    private static readonly Color DarkTitleBarButtonHoverForegroundColor = ColorHelper.FromArgb(255, 28, 28, 28);
+    private static readonly Color DarkTitleBarButtonPressedForegroundColor = ColorHelper.FromArgb(255, 18, 18, 18);
     private static readonly Color DarkTitleBarInactiveForegroundColor = ColorHelper.FromArgb(255, 176, 176, 176);
-    private static readonly Color DarkTitleBarButtonHoverBackgroundColor = ColorHelper.FromArgb(34, 255, 255, 255);
-    private static readonly Color DarkTitleBarButtonPressedBackgroundColor = ColorHelper.FromArgb(58, 255, 255, 255);
+    private static readonly Color DarkTitleBarButtonHoverBackgroundColor = ColorHelper.FromArgb(255, 224, 224, 224);
+    private static readonly Color DarkTitleBarButtonPressedBackgroundColor = ColorHelper.FromArgb(255, 206, 206, 206);
 
     private readonly AppWindow _appWindow;
 
@@ -63,8 +69,11 @@ public sealed partial class MainWindow : Window
         var isDarkTheme = RootLayout.ActualTheme == ElementTheme.Dark;
         var backgroundColor = isDarkTheme ? DarkTitleBarBackgroundColor : LightTitleBarBackgroundColor;
         var foregroundColor = isDarkTheme ? DarkTitleBarForegroundColor : LightTitleBarForegroundColor;
+        var buttonForegroundColor = isDarkTheme ? DarkTitleBarButtonForegroundColor : LightTitleBarButtonForegroundColor;
         var inactiveForegroundColor = isDarkTheme ? DarkTitleBarInactiveForegroundColor : LightTitleBarInactiveForegroundColor;
+        var hoverForegroundColor = isDarkTheme ? DarkTitleBarButtonHoverForegroundColor : LightTitleBarButtonHoverForegroundColor;
         var hoverBackgroundColor = isDarkTheme ? DarkTitleBarButtonHoverBackgroundColor : LightTitleBarButtonHoverBackgroundColor;
+        var pressedForegroundColor = isDarkTheme ? DarkTitleBarButtonPressedForegroundColor : LightTitleBarButtonPressedForegroundColor;
         var pressedBackgroundColor = isDarkTheme ? DarkTitleBarButtonPressedBackgroundColor : LightTitleBarButtonPressedBackgroundColor;
 
         var titleBar = _appWindow.TitleBar;
@@ -73,13 +82,13 @@ public sealed partial class MainWindow : Window
         titleBar.InactiveBackgroundColor = backgroundColor;
         titleBar.InactiveForegroundColor = inactiveForegroundColor;
         titleBar.ButtonBackgroundColor = backgroundColor;
-        titleBar.ButtonForegroundColor = foregroundColor;
+        titleBar.ButtonForegroundColor = buttonForegroundColor;
         titleBar.ButtonInactiveBackgroundColor = backgroundColor;
         titleBar.ButtonInactiveForegroundColor = inactiveForegroundColor;
         titleBar.ButtonHoverBackgroundColor = hoverBackgroundColor;
-        titleBar.ButtonHoverForegroundColor = foregroundColor;
+        titleBar.ButtonHoverForegroundColor = hoverForegroundColor;
         titleBar.ButtonPressedBackgroundColor = pressedBackgroundColor;
-        titleBar.ButtonPressedForegroundColor = foregroundColor;
+        titleBar.ButtonPressedForegroundColor = pressedForegroundColor;
     }
 
     private void ConfigureWindowConstraints()
